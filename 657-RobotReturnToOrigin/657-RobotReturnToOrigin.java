c3d82dc9f390dd522fbbef1a366abad2eaf360d2
pandilakshmi20062510-dotcom <pandilakshmi20062510@gmail.com>
2026-08-12 20:34:56 +0530

    Create 657-RobotReturnToOrigin

---
+++ 657-RobotReturnToOrigin/657-RobotReturnToOrigin.java
@@ -0,0 +1,21 @@
+// Last updated: 12/08/2026, 20:34:55
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x = 0, y = 0;  
+
+        for (char c : moves.toCharArray()) {
+            switch (c) {
+                case 'U': y += 1; break;
+                case 'D': y -= 1; break;
+                case 'L': x -= 1; break;
+                case 'R': x += 1; break;
+            }
+        }
+
+        if (x == 0 && y == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
