f01514a5e7451f2fd08959f576b01bae46a6228b
pandilakshmi20062510-dotcom <pandilakshmi20062510@gmail.com>
2026-08-12 20:29:08 +0530

    Create 2195-TimeNeededToBuyTickets

---
+++ 2195-TimeNeededToBuyTickets/2195-TimeNeededToBuyTickets.java
@@ -0,0 +1,19 @@
+// Last updated: 12/08/2026, 20:29:07
+class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0;i<tickets.length;i++)
+           q.add(i);
+        int t =0;
+        while(!q.isEmpty()){
+            int ind = q.poll();
+            t++;
+            tickets[ind]--;
+            if(tickets[ind]>0)
+               q.add(ind);
+            if(ind==k && tickets[ind]==0)
+               return t;
+        }
+        return t;
+    }
+}
