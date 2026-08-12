d7da7dad27cc5e45824d5a439c5599cd18ef85f4
pandilakshmi20062510-dotcom <pandilakshmi20062510@gmail.com>
2026-08-12 20:28:35 +0530

    Create 2654-CountTheNumberOfVowelStringsInRange

---
+++ 2654-CountTheNumberOfVowelStringsInRange/2654-CountTheNumberOfVowelStringsInRange.java
@@ -0,0 +1,24 @@
+// Last updated: 12/08/2026, 20:28:34
+class Solution {
+    public int vowelStrings(String[] words, int left, int right) {
+
+        int count = 0;
+
+        while (left <= right) {
+
+            String s = words[left];
+
+            char start = s.charAt(0);
+            char end = s.charAt(s.length() - 1);
+
+            if ((start == 'a' || start == 'e' || start == 'i' || start == 'o' || start == 'u')
+                    && (end == 'a' || end == 'e' || end == 'i' || end == 'o' || end == 'u')) {
+                count++;
+            }
+
+            left++;
+        }
+
+        return count;
+    }
+}
