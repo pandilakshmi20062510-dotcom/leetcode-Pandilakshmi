585bbcfc164c37020901a5f459b8118ddf848dcc
pandilakshmi20062510-dotcom <pandilakshmi20062510@gmail.com>
2026-08-12 20:36:34 +0530

    Create 504-Base7

---
+++ 504-Base7/504-Base7.java
@@ -0,0 +1,25 @@
+// Last updated: 12/08/2026, 20:36:34
+class Solution {
+    public String convertToBase7(int num) {
+        return Integer.toString(num, 7);
+    }
+}
+// class Solution {
+//     public String convertToBase7(int num) {
+//         if (num == 0) return "0";
+
+//         boolean negative = num < 0;
+//         num = Math.abs(num);
+//         StringBuilder sb = new StringBuilder();
+
+//         while (num > 0) {
+//             int rem = num % 7;
+//             sb.append(rem);   // append remainder
+//             num /= 7;         // divide by 7
+//         }
+
+//         if (negative) sb.append('-');
+
+//         return sb.reverse().toString();
+//     }
+// }
