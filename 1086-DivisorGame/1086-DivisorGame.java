610611edfce2f9424627a85b8b8cabd2238b9839
pandilakshmi20062510-dotcom <pandilakshmi20062510@gmail.com>
2026-08-12 20:32:24 +0530

    Create 1086-DivisorGame

---
+++ 1086-DivisorGame/1086-DivisorGame.java
@@ -0,0 +1,12 @@
+// Last updated: 12/08/2026, 20:32:24
+class Solution {
+    public boolean divisorGame(int n) {
+       if(n % 2 == 0)
+       return true;
+       else
+       return false;
+
+       
+    }
+} 
+    
