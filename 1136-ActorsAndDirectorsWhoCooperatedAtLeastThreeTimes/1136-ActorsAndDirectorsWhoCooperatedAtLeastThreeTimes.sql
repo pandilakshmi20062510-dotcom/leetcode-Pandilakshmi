f17dcdf8e1ed438f861af6fc151f01f6ad1afa1f
pandilakshmi20062510-dotcom <pandilakshmi20062510@gmail.com>
2026-08-12 20:32:11 +0530

    Create 1136-ActorsAndDirectorsWhoCooperatedAtLeastThreeTimes

---
+++ 1136-ActorsAndDirectorsWhoCooperatedAtLeastThreeTimes/1136-ActorsAndDirectorsWhoCooperatedAtLeastThreeTimes.sql
@@ -0,0 +1,10 @@
+-- Last updated: 12/08/2026, 20:32:10
+# Write your MySQL query statement below
+select actor_id, director_id 
+from(
+select actor_id,director_id, 
+count(timestamp) as cooperated 
+from ActorDirector 
+group by actor_id,director_id) 
+table1
+where cooperated>=3;
