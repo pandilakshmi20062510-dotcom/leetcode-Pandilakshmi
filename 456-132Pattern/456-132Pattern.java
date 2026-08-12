3119fec4c73eaa5627778d26a0fadee41514e159
pandilakshmi20062510-dotcom <pandilakshmi20062510@gmail.com>
2026-08-12 20:36:55 +0530

    Create 456-132Pattern

---
+++ 456-132Pattern/456-132Pattern.java
@@ -0,0 +1,19 @@
+// Last updated: 12/08/2026, 20:36:54
+class Solution {
+    public boolean find132pattern(int[] nums) {
+        int secondMax = Integer.MIN_VALUE;
+
+        Stack<Integer> stack = new Stack<>();
+        for(int i=nums.length-1; i>=0; i--){
+            if(nums[i] < secondMax){
+                return true;
+            }
+
+            while(!stack.isEmpty() && nums[i] > stack.peek()){
+                secondMax = Math.max(secondMax, stack.pop());
+            }
+            stack.push(nums[i]);
+        }
+        return false;
+    }
+}
