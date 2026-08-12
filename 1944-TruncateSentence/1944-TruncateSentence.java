fdc8fbaf08c723b62c126f3147fd0d25ac95a917
pandilakshmi20062510-dotcom <pandilakshmi20062510@gmail.com>
2026-08-12 20:29:43 +0530

    Create 1944-TruncateSentence

---
+++ 1944-TruncateSentence/1944-TruncateSentence.java
@@ -0,0 +1,15 @@
+// Last updated: 12/08/2026, 20:29:42
+class Solution {
+    public String truncateSentence(String s, int k) {
+                    int i=0;
+        while ( i < s.length()) {
+            if (s.charAt(i++) == ' ') {
+                k--;
+                if (k == 0)
+                    break;
+            }
+        }
+           if(i==s.length())
+        return  s.substring (0,i);
+           return s.substring(0,i-1);
+}}
