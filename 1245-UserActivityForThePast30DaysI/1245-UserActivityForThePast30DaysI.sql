dce17889ce6e6d5632a81139299aea8805f3eb4b
pandilakshmi20062510-dotcom <pandilakshmi20062510@gmail.com>
2026-08-12 20:31:42 +0530

    Create 1245-UserActivityForThePast30DaysI

---
+++ 1245-UserActivityForThePast30DaysI/1245-UserActivityForThePast30DaysI.sql
@@ -0,0 +1,7 @@
+-- Last updated: 12/08/2026, 20:31:41
+SELECT activity_date AS day,
+       COUNT(DISTINCT user_id) AS active_users
+FROM Activity
+WHERE DATE_SUB('2019-07-27', INTERVAL 30 DAY) < activity_date
+  AND activity_date <= '2019-07-27'
+GROUP BY activity_date;
