e21760156ffc7b21cc39890cb2d40388b9a4abde
pandilakshmi20062510-dotcom <pandilakshmi20062510@gmail.com>
2026-08-12 20:30:04 +0530

    Create 1734-BankAccountSummaryIi

---
+++ 1734-BankAccountSummaryIi/1734-BankAccountSummaryIi.sql
@@ -0,0 +1,10 @@
+-- Last updated: 12/08/2026, 20:30:03
+# Write your MySQL query statement below
+SELECT  
+    u.name,
+    SUM(t.amount) AS balance
+FROM Transactions t
+LEFT JOIN Users u
+    ON t.account = u.account
+GROUP BY u.account, u.name
+HAVING SUM(t.amount) > 10000;
