b1c476985061d0ac068702f989ee84da06cd7f7a
pandilakshmi20062510-dotcom <pandilakshmi20062510@gmail.com>
2026-08-12 20:34:20 +0530

    Create 742-ToLowerCase

---
+++ 742-ToLowerCase/742-ToLowerCase.java
@@ -0,0 +1,7 @@
+// Last updated: 12/08/2026, 20:34:19
+class Solution {
+    public String toLowerCase(String s) {
+        // Simple 1-liner relying on Java's built-in locale-aware string manipulation
+        return s.toLowerCase();
+    }
+}
