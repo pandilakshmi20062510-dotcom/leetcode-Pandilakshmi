f1420de376a9be7a64f6db0be9a9e5a3d76522ed
pandilakshmi20062510-dotcom <pandilakshmi20062510@gmail.com>
2026-08-12 20:35:54 +0530

    Create 602-FriendRequestsIiWhoHasTheMostFriends

---
+++ 602-FriendRequestsIiWhoHasTheMostFriends/602-FriendRequestsIiWhoHasTheMostFriends.sql
@@ -0,0 +1,16 @@
+-- Last updated: 12/08/2026, 20:35:53
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+select id, count(*) as num
+from (
+select requester_id as id
+from RequestAccepted
+
+union all
+
+select accepter_id as id
+from RequestAccepted
+) as friends
+group by id
+order by num desc
+limit 1;
